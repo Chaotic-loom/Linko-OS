0c960a9cbcaeb4c35ef986595a2464765fe42b7e
restonic4 <marcosruizlerma@gmail.com>
2025-05-15 11:20:43 +0200

    autologin

--- image/mbr/simple_dual/post-build.sh
+++ image/mbr/simple_dual/post-build.sh
@@ -4,12 +4,11 @@ set -eu
 
 ROOTFS=$1
 
-# Auto login
-
+# configure autologin
 sudo mkdir -p ${ROOTFS}/etc/systemd/system/getty@tty1.service.d
-sudo /bin/sh -c "cat <<EOF > ${ROOTFS}/etc/systemd/system/getty@tty1.service.d/override.conf"
+sudo /bin/sh -c "cat <<EOF > ${ROOTFS}/etc/systemd/system/getty@tty1.service.d/override.conf
 [Service]
 ExecStart=
-ExecStart=-/sbin/agetty --noclear --autologin linko %I $TERM
+ExecStart=-/sbin/agetty --noclear --autologin macmind %I $TERM
 Type=idle
 EOF"
