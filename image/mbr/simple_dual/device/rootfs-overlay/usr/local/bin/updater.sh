10fe85e361cf026720406d011ee863ef1b403a90
restonic4 <marcosruizlerma@gmail.com>
2025-05-19 08:47:20 +0200

    Update loader

---
+++ image/mbr/simple_dual/device/rootfs-overlay/usr/local/bin/updater.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+LAUNCHER_DIR="/opt/linko/launcher"
+UPDATE_JAR="$LAUNCHER_DIR/Update.jar"
+TARGET_JAR="$LAUNCHER_DIR/LinkoLauncher.jar"
+FLAG_FILE="/boot/updated"
+
+if [[ ! -d "$LAUNCHER_DIR" ]]; then
+  echo "Error: Directory $LAUNCHER_DIR not found."
+  exit 1
+fi
+
+if [[ ! -f "$UPDATE_JAR" ]]; then
+  echo "Update.jar not found on $LAUNCHER_DIR."
+  exit 0
+fi
+
+if [[ -f "$TARGET_JAR" ]]; then
+  echo "Deleting $TARGET_JAR..."
+  rm -f "$TARGET_JAR"
+  if [[ $? -ne 0 ]]; then
+    echo "Error deleting $TARGET_JAR."
+    exit 1
+  fi
+fi
+
+echo "Renaming Update.jar to LinkoLauncher.jar..."
+mv "$UPDATE_JAR" "$TARGET_JAR"
+if [[ $? -ne 0 ]]; then
+  echo "Error renaming $UPDATE_JAR."
+  exit 1
+fi
+
+touch "$FLAG_FILE" || {
+  echo "Error creating $FLAG_FILE." >&2
+  exit 1
+}
+
+echo "Updated!"
+
+exit 0
