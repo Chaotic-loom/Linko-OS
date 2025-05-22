b461748c9e414224f25106ca945fc096b111c3e7
restonic4 <marcosruizlerma@gmail.com>
2025-05-22 11:44:53 +0200

    cleanup

--- image/mbr/simple_dual/device/rootfs-overlay/usr/local/bin/first-boot.sh
+++ image/mbr/simple_dual/device/rootfs-overlay/usr/local/bin/first-boot.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-# Grant permissions
-usermod -a -G video,render,netdev player
-chmod 755 /opt/linko/
 
-# Expand rootfs (Expand sd card size)
+# Only if the flag does not exists
 if [ ! -f /boot/firstboot_done ]; then
+    # Expand rootfs (Expand sd card size)
     raspi-config --expand-rootfs
+
+    # Grant permissions
+    usermod -a -G video,render,netdev player
+    chmod 755 /opt/linko/
+
+    # Create flag and reboot
     touch /boot/firstboot_done
     sync
     echo b > /proc/sysrq-trigger
