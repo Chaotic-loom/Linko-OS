9cf36b7b2ccac1c4ac02312257b56bbe3f64a8f8
restonic4 <marcosruizlerma@gmail.com>
2025-05-22 11:42:04 +0200

    cleanup

--- image/mbr/simple_dual/device/rootfs-overlay/usr/local/bin/first-boot.sh
+++ image/mbr/simple_dual/device/rootfs-overlay/usr/local/bin/first-boot.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# Grant permissions
+usermod -a -G video,render,netdev player
+chmod 755 /opt/linko/
+
+# Expand rootfs (Expand sd card size)
 if [ ! -f /boot/firstboot_done ]; then
     raspi-config --expand-rootfs
     touch /boot/firstboot_done
