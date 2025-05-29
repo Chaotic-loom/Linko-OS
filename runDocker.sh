2d4cd6caac9c1115be70d595cf15a29730ee1d06
restonic4 <marcosruizlerma@gmail.com>
2025-05-30 00:04:46 +0200

    Updated docker settings

--- runDocker.sh
+++ runDocker.sh
@@ -2,7 +2,21 @@
 
 sudo systemctl start docker
 
-sudo docker compose build
-sudo docker compose run --rm rpi_imagegen bash
+sudo docker compose down \
+  --rmi local
+  --volumes
+  --remove-orphans
+
+docker system prune -a --volumes
+
+sudo docker compose build --no-cache
+sudo docker compose run --rm \
+  --entrypoint bash \
+  rpi_imagegen -ic "\
+    cd Linko-OS && \
+    ./linko-build.sh && \
+    cp work/linko/deploy/linko.img /host-downloads/linko.img && \
+    exec bash"
+
 
 sudo systemctl stop docker
